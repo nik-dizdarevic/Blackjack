1f1551fab4ed1c23f023167f554c2dc6412be9e5
nikdizdarevic <navdux@gmail.com>
2022-05-03 10:33:20 +0200

    Initial Commit

---
+++ Blackjack/BlackjackApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlackjackApp.swift
+//  Blackjack
+//
+//  Created by Nik Dizdarevic on 03/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct BlackjackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
